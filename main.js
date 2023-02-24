97bfcf8f58108cc33bc9f8b602479eba3ef96e41
Wendesday36 <bertazoeanna@gmail.com>
2023-02-24 12:27:22 +0100

    alap

---
+++ main.js
@@ -0,0 +1,17 @@
+window.addEventListener("load",init);
+
+function init(){
+    const tartalomElem = document.getElementById("tartalom")
+    console.log(tartalomElem);
+
+    const tartalomElemMaskepp = document.querySelector("#tartalom")
+    const tartalomElemMaskepp2 = document.querySelector(".tart")
+    const tartalomElemMaskepp3 = document.getElementsByClassName("tart")
+    const tartalomElemEz = document.querySelectorAll(".tart")
+    console.log(tartalomElemEz)
+
+
+
+    /**készits egy listat a kedvenc eteleid nevevel */
+    /**ird ki a tartalom divbe felsoroloásba*/
+}
