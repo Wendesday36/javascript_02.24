4fc583e1a531ce990c956ffd98d4e9aa2a9fc6ec
Wendesday36 <bertazoeanna@gmail.com>
2023-02-24 13:44:16 +0100

    flex

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ function init() {
   const tartalomElem = document.getElementById("tartalom");
   console.log(tartalomElem);
   const tartalomElemek = document.getElementsByClassName("kepek");
-  const tartalomElemMaskepp = document.querySelector("#tartalom");
-  const tartalomElemMaskepp2 = document.querySelector(".tart");
-  const tartalomElemMaskepp3 = document.getElementsByClassName("tart");
+  console.log(tartalomElemek);
+  //const tartalomElemMaskepp = document.querySelector("#tartalom");
+  // const tartalomElemMaskepp2 = document.querySelector(".tart");
+  //const tartalomElemMaskepp3 = document.getElementsByClassName("tart");
   const tartalomElemEz = document.querySelectorAll(".tart");
   console.log(tartalomElemEz);
   console.log(tartalomElemEz[1]);
@@ -19,7 +20,7 @@ function init() {
 
   let lista = ["pizza", "csoki", "nemtudom", "valami"];
   etelek(lista, tartalomElemEz);
-  etelekepek( tartalomElemek);
+  etelkepek(tartalomElemek);
   /**készits egy listat a kedvenc eteleid nevevel */
   /**ird ki a tartalom divbe felsoroloásba*/
 }
@@ -32,12 +33,13 @@ function etelek(lista, tartalomElemEz) {
   szoveg += "</ul>";
   tartalomElemEz[0].innerHTML = szoveg;
 }
-function etelekepek( tartalomElemek) {
-  var img = document.createElement("img");
-  img.src =
-    "https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D329579847426939&imgrefurl=https%3A%2F%2Fwww.facebook.com%2Fkajapara1%2F%3Flocale%3Dhu_HU&tbnid=n_DYiguqL05inM&vet=12ahUKEwjEnaz6jq79AhVqg_0HHfHUAeYQMygaegUIARD3AQ..i&docid=kLLxlGVTSJBN5M&w=640&h=640&q=kaja%20kepek&ved=2ahUKEwjEnaz6jq79AhVqg_0HHfHUAeYQMygaegUIARD3AQ";
-  for (let index = 0; index++;) {
-    tartalomElemek.appendChild(img);
-    console.log(img);
+function etelkepek(tartalomElemek) {
+  let kepekLista = ["fnjdk", "fnjdk", "fnjdk", "fnjdk"];
+  for (let index = 0; index < kepekLista.length; index++) {
+    let img = document.createElement("img");
+    img.src =
+      "https://receptexpressz.hu/wp-content/uploads/2017/02/party-etel-kaja-leveles-tesza-virsli-rolad2-e1487848253564.jpg";
+    tartalomElemek[0].appendChild(img);
+    console.log(tartalomElemek);
   }
 }
